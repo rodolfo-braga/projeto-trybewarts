babe5fc3920d1baf9a313b72023a8398cd4fbde4
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-21 22:38:53 -0300

    Requisito 18 completo

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const Email = document.getElementById('email-btn');
 const Senha = document.getElementById('senha-btn');
 const Submit = document.getElementById('login-btn');
+const submitButton = document.getElementById('submit-btn');
+const checkbox = document.getElementById('agreement');
 
 Submit.addEventListener('click', () => {
   if (Email.value === 'tryber@teste.com' && Senha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Login ou senha inválidos.');
+  }
+});
+
+checkbox.addEventListener('click', () => {
+  if (submitButton.disabled === true) {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
   }
 });
