6e50a91fcdc886f4c7558ae7582df0a047d241fe
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-07-21 21:27:43 -0300

    Requisito 17 completo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const Email = document.getElementById('email-btn');
 const Senha = document.getElementById('senha-btn');
-const Submit = document.getElementById('submit-btn');
+const Submit = document.getElementById('login-btn');
 
 Submit.addEventListener('click', () => {
   if (Email.value === 'tryber@teste.com' && Senha.value === '123456') {
